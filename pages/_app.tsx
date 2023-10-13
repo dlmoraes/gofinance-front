349229636a0dbee1ff0ec0af289af26924ed0a5f
Diego Moraes <diegofrost96@gmail>
2023-10-13 14:36:42 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import GlobalStyles from "@/styles/global";
+import { darkTheme } from "@/styles/themes/dark";
+import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
